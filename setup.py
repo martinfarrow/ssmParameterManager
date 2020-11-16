521950533f6bcdc135edb43e24bdcb3df2271b20
Martin Farrow <martin.farrow@versacloud.com>
2020-11-16 18:31:34 +0000

    master | @martinfarrow | converting to package

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, find_packages
+
+setup(name='ssmParameterManager',
+      version='0.1',
+      description='A module with command line utility to manipulate parameter store entries',
+      author='Martin Farrow',
+      author_email='ssmParameterManager@dibley.net',
+      py_modules=['param'],
+      license='LICENSE',
+    )
